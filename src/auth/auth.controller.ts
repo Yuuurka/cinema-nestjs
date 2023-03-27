848e715c94f446536d0664e327e78bea8bfef851
Yuuurka <Yuryukito@yandex.ru>
2023-03-28 01:16:48 +0300

    Добавлен эндопоинт на логин

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,21 @@
-import {Controller, Get} from '@nestjs/common';
+import {Body, Controller, Get, Post} from '@nestjs/common';
 import {AuthService} from "./auth.service";
+import {ApiTags} from "@nestjs/swagger";
+import {CreateUserDto} from "./dto/create-user.dto";
 
+@ApiTags('Авторизация')
 @Controller('auth')
 export class AuthController {
     constructor(private readonly authService: AuthService) {
     }
 
-    @Get()
-    getUsers(){
-        return this.authService.getUsers()
+    @Post('/login')
+    login(@Body() userDto: CreateUserDto){
+        return this.authService.login(userDto)
+    }
+
+    @Post('/registration')
+    registration(@Body() userDto: CreateUserDto){
+        return this.authService.registration(userDto)
     }
 }
