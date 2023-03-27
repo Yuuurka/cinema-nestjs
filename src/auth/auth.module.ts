3eaffbe8fe5f942611ab2648481005b4cef3c9a1
Yuuurka <Yuryukito@yandex.ru>
2023-03-28 01:21:10 +0300

    Добавлен модуль JWT

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,10 +2,16 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import {DatabaseModule} from "../database/database.module";
 import {AuthController} from "./auth.controller";
+import {JwtModule} from "@nestjs/jwt";
 
 @Module({
   providers: [AuthService],
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, JwtModule.register({
+    secret: process.env.PRIVATE_KEY || 'SECRET',
+    signOptions: {
+      expiresIn: '24h'
+    }
+  })],
   controllers: [AuthController]
 })
 export class AuthModule {}
