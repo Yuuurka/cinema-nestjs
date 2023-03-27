afdc861939f4419699fa48f728b4131ce439433d
Yuuurka <Yuryukito@yandex.ru>
2023-03-28 01:14:51 +0300

    init commit

---
+++ src/auth/dto/create-user.dto.ts
@@ -0,0 +1,8 @@
+export class CreateUserDto{
+    readonly login: string;
+    readonly password: string;
+    readonly isAdmin?: boolean;
+    readonly name?: string;
+    readonly fam?: string;
+    readonly phone_number?: string;
+}
