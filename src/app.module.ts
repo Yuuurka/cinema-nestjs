4f42a2fd1427d761f08c1501fb4fa3640aafc880
Yuuurka <Yuryukito@yandex.ru>
2023-03-29 02:35:11 +0300

    импорт кабинета

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthController } from './auth/auth.controller';
 import { AuthModule } from './auth/auth.module';
+import { CabinetModule } from './cabinet/cabinet.module';
 
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, CabinetModule],
   controllers: [AppController],
-  providers: [AppService, AppService],
+  providers: [AppService],
 })
 export class AppModule {}
