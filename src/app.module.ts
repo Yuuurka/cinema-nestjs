49a4ed56c284928a534943fa8f893daea763abb4
Yuuurka <Yuryukito@yandex.ru>
2023-03-30 20:03:26 +0300

    добавлены связи

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,25 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { CabinetModule } from './cabinet/cabinet.module';
+import {AdminPanelModule} from "./admin-panel/admin-panel.module";
+import { TextBlockModule } from './text-block/text-block.module';
+import {TypeOrmModule} from "@nestjs/typeorm";
 
 
 @Module({
-  imports: [AuthModule, CabinetModule],
+  imports: [AuthModule, CabinetModule, AdminPanelModule, TextBlockModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'auth_hm3',
+      entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
