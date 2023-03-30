6b3bfaac3cdf27379ff062ba04bc3f2e306ba1f6
Yuuurka <Yuryukito@yandex.ru>
2023-03-30 20:04:03 +0300

    isAdmin - значение по умолчанию

--- src/auth/dto/create-user.dto.ts
+++ src/auth/dto/create-user.dto.ts
@@ -1,7 +1,7 @@
 export class CreateUserDto{
     readonly login: string;
     readonly password: string;
-    readonly isAdmin?: boolean;
+    readonly isAdmin?: boolean = false;
     readonly name?: string;
     readonly fam?: string;
     readonly phone_number?: string;
